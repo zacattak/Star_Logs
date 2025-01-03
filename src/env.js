ad6d887d4e50c325978d70c6207abe35f9ede7a4
zac <step7476@alumni.uidaho.edu>
2025-01-03 08:19:34 -0700

    established env.js for configuration with sandbox api

---
+++ src/env.js
@@ -0,0 +1,6 @@
+export const useSockets = false
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = 'https://sandbox.codeworksacademy.com/'
+export const domain = 'codeworksclassroom.auth0.com'
+export const audience = 'https://codeworksclassroom.com'
+export const clientId = 'pOXw2OGv1LsYi7LEBmDF04RLkXQvldml'
