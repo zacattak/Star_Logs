edc6d282ebc992a87a9e952fd43f43376e8cfffd
zac <step7476@alumni.uidaho.edu>
2025-01-09 14:02:31 -0700

    got rid of preset styling in App.tsx (navbar, footer)

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
 import { Outlet } from 'react-router-dom'
-import { Navbar } from './components/Navbar.js'
+
 
 export function App() {
 
   return (
     <div className="App" id="app">
-      <header>
+      {/* <header>
         <Navbar />
-      </header>
+      </header> */}
 
       <main>
         <Outlet />
       </main>
 
-      <footer className="bg-dark text-light text-center p-3 ">
-        Made with 💖 by CodeWorks
-      </footer>
+      {/* <footer className="bg-dark text-light text-center p-3 ">
+
+      </footer> */}
 
     </div>
   )
